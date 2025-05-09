c6614a53c70e187e71e1f409135ea9b8667681e8
Noberto85 <nobertorafael9@gmail.com>
2025-05-09 16:07:41 -0300

    primeiro commit

---
+++ src/test/java/com/tokiomarine/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
